eee92df01620ccbb1575bb01f17de399a89ce269
xinwang <wangxin8923@gmail.com>
2019-02-14 23:51:43 -0500

    complete simulation function need to check

--- simdat.jl
+++ simdat.jl
@@ -1,65 +1,60 @@
 ##### simulated data ###
 ## two groups
+using DataFrames
+using Random
+using StatsBase
+using Statistics
 
 function m1(x::Number)
 4*(x - 0.5)^2 + 1
-end
-
-function m1(x::Vector)
-    n = length(x)
-    value = zeros(n)
-    for i = 1:n
-        value[i] = 4*(x[i] - 0.5)^2 + 1
-    end
-    return value
 end
 
 function m2(x::Number)
     2.5*exp(-25*(x - 0.25)^2) + 2*exp(-50*(x - 0.75)^2)
 end
 
-
-function m2(x::Vector)
-    n = length(x)
-    value = zeros(n)
-    for i = 1:n
-        value[i] = 2.5*exp(-25*(x[i] - 0.25)^2) + 2*exp(-50*(x[i] - 0.75)^2)
-    end
-    return(value)
-end
-
 ## eigenfunctions
-
 function psi2(x::Number)
     sqrt(2)*sin(2*pi*x)
 end
-
-function psi2(x::Vector)
-    n = length(x)
-    value = zeros(n)
-    for i = 1:n
-        value[i] = sqrt(2)*sin(2*pi*x[i])
-    end
-    return value
-end
-
 
 function psi3(x::Number)
     sqrt(2)*cos(2*pi*x)
 end
 
-function psi3(x::Vector)
-    n = length(x)
-    value = zeros(n)
-    for i = 1:n
-        value[i] = sqrt(2)*cos(2*pi*x[i])
-    end
-    return value
-end
+
 
 ## equal spaced grid
 ## ncl: number of observations for each cluster, assume same now.
 ## the output is a tuple, first element is about data column information,
 ## the second element is about the data, the third element is the group information
-function simdat(sig2::Number, lamj::Vector, m::Int, ncl::Int)
+function simdat(sig2::Number, lamj::Vector;
+    m::Int = 20, ncl::Int = 50, seed::Int = 2228)
+    Random.seed!(seed)
     tvec = collect(range(0, length = m, stop = 1))
+
+    ntotal = 2 * ncl * m
+
+    data = DataFrame(group = repeat(1:2,inner = m*ncl),
+             ind = repeat(1:(2*ncl),inner = m),
+             time = repeat(tvec, ncl*2),
+             obs = zeros(ntotal))
+
+    rande = sig2 * randn(ntotal)
+
+    for i = 1:(ntotal)
+        ti = data.time[i]
+        groupi = data.group[i]
+        vi = lamj[1]*randn(1)[1] + lamj[2]*randn(1)[1]*psi2(ti) +
+                lamj[3]*randn(1)[1]*psi3(ti)
+
+        if groupi== 1
+            data.obs[i] = m1(ti) + vi
+        else
+            data.obs[i] = m2(ti) + vi
+        end
+    end
+
+    data.obs = data.obs .+ rande
+    return data
+end
