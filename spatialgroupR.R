1d4ab149619f8cd106ae9c3b3735444c9dfaf54c
Wang <wangx172@STAXWang172MBP2.local>
2019-06-09 20:22:47 -0700

    revise

---
+++ spatialgroupR.R
@@ -0,0 +1,25 @@
+gridm <- expand.grid(12:1,1:12)[,2:1]
+
+f1 <- function(x)
+{
+  0.7*x[1] + x[2] - 13
+}
+
+f2 <- function(x)
+{
+  0.75*x[1] - x[2]
+}
+
+value1 <- apply(gridm,1,f1)
+value2 <- apply(gridm, 1, f2)
+
+group <- rep(0, 12*12)
+
+group[value1<0 & value2 <=0& gridm[,1]<7] <- 1
+group[ gridm[,2] >=7 & group==0] <- 2
+group[group==0] <- 3
+
+table(group)
+
+qplot(x = gridm[,1],y = gridm[,2], colour = as.factor(group))
+
