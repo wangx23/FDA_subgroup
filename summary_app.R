415efe5056799d3c6b06251ec95eee2fdae09b2e
Wang <wangx172@ip-172-20-10-171.dhcp.muohio.edu>
2019-09-10 23:37:19 -0400

    summary of app

---
+++ summary_app.R
@@ -0,0 +1,37 @@
+
+library(tidyverse)
+dat = read.csv("Dropbox/Tanja&XinW/Newdata/AggObese1990_2017.csv")
+dat = dat[dat$AGE!=80,]
+
+
+pdf("Google Drive File Stream/My Drive/Research/FDA_subgroup/doc/figures/datapattern.pdf",width = 5,height = 4)
+ggplot(data = dat, aes(x = IYEAR, y = PropObese, group = AGE, color = AGE)) +
+  geom_line() + 
+  labs(x = "Year",y = "Proportion of Obesity") +
+  theme_bw()
+dev.off()
+
+groupest = read.table("Google Drive File Stream/My Drive/Research/FDA_subgroup/result/groupest.txt")
+
+
+
+
+meanfunest = read.table("Google Drive File Stream/My Drive/Research/FDA_subgroup/result/meanfunest52.txt")
+
+datpred = dat
+datpred$pred = meanfunest$V1
+datpred = arrange(datpred, AGE, IYEAR) %>%
+  mutate(groupest = rep(groupest$V1,each = length(unique(datpred$IYEAR))))
+
+
+ggplot(data = datpred) + 
+  geom_line(aes(x = IYEAR, y = PropObese, group = AGE)) +
+  geom_line(aes(x = IYEAR, y = pred, color = "red")) +
+  facet_wrap(~groupest, ncol = 4) +
+  labs( x= "Year", y = "Proportion of Obesity") + 
+  theme_bw() +
+  theme(legend.position = "none")
+
+##### figure #####
+
+
